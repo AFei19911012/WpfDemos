aa9d906eee65b3da473cee765cd24bfacef4dd97
Big Wang <1105936347@qq.com>
2024-07-18 23:56:36 +0800

    w+

---
+++ 043_InputSimulatorDemo/InputSimulatorDemo/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WindowsInput;
+using Button = System.Windows.Controls.Button;
+
+namespace InputSimulatorDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private InputSimulator Simulator { get; set; } = new InputSimulator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Focus();
+
+            string btn = (sender as Button).Content.ToString();
+            if (btn.EndsWith("数字"))
+            {
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.RETURN);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_1);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_2);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_3);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.DECIMAL);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_4);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_5);
+            }
+            else if (btn.EndsWith("字母"))
+            {
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.RETURN);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_B);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_I);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_G);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.SPACE);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_W);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_A);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_N);
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_G);
+            }
+            else if (btn.EndsWith("文本"))
+            {
+                Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.RETURN);
+                Simulator.Keyboard.TextEntry("Hello, Big Wang");
+            }
+            else
+            {
+                Task.Run(() =>
+                {
+                    Simulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.RETURN);
+                    Simulator.Keyboard.TextEntry("现在，移动鼠标，然后点击左键");
+
+                    Thread.Sleep(1000);
+
+                    
+                    Dispatcher.Invoke(() =>
+                    {
+                        double x = Left + 200;
+                        double y = Top + 100;
+
+                        double dh = 65535 / SystemParameters.PrimaryScreenHeight;
+                        double dw = 65535 / SystemParameters.PrimaryScreenWidth;
+
+                        Simulator.Mouse.MoveMouseTo(x * dw, y * dh);
+                        Simulator.Mouse.RightButtonClick();
+                    });
+                });
+                
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -38,8 +38,10 @@ WPF 测试项目，第三方库使用案例
 35. ParsingHelper 文本解析方法；
 36. OrderedDictionary 有序字典；
 37. ExpressionEvaluator 表达式计算；
-38. FixedWidthParser 指定宽度数据文件读写
-39. EasyEncryption 加密
-40. WinSettings 加密保存 ini、xml、注册表
-41. SimpleLogFile 简单日志
-42. 
+38. FixedWidthParser 指定宽度数据文件读写；
+39. EasyEncryption 加密；
+40. WinSettings 加密保存 ini、xml、注册表；
+41. SimpleLogFile 简单日志；
+42. SpreadsheetBuilder 轻量级 xlsx 文件读写；
+43. InputSimulator 模拟键盘输入；
+44. 
