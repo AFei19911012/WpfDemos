799c7d19dfa99386ab0efee44e83b943bc429fad
Big Wang <1105936347@qq.com>
2025-02-24 02:20:42 +0800

    Dommel

--- 028_DapperDemo/MainWindow.xaml.cs
+++ 028_DapperDemo/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Dapper;
+using Dommel;
 using Microsoft.Data.Sqlite;
+using System.Data.Common;
 using System.Windows;
 
 namespace DapperDemo
@@ -14,6 +16,8 @@ namespace DapperDemo
             InitializeComponent();
 
             Init();
+
+            InitDommelAsync();
         }
 
         private void Init()
@@ -40,6 +44,32 @@ namespace DapperDemo
             {
                 lb.Items.Add(item.ToString());
             }
+        }
+
+        private async void InitDommelAsync()
+        {
+            // 插入数据
+            using var con = new SqliteConnection("Data Source=Dommel.db");
+            // 创建表
+            con.Execute("CREATE TABLE IF NOT EXISTS Persons (Id INTEGER PRIMARY KEY, Name TEXT)");
+            con.Open();
+            var user = new Person { Name = "John Doe" };
+            var id = con.Insert(user);
+
+            var p = con.Get<Person>(1);
+
+            var products = con.Select<Person>(p => p.Name == "John Doe");
+
+            var ps = con.GetAll<Person>();
+            lb.Items.Add("");
+            lb.Items.Add("查询数据库");
+            foreach (var item in ps)
+            {
+                lb.Items.Add(item.ToString());
+            }
+
+            var peo = await con.GetAsync<Person>(1);
+            var peos = await con.SelectAsync<Person>(p => p.Name.Contains("John"));
         }
     }
 
